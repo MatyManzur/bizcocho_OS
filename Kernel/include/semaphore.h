d440f1c90ee0693e8fe4c3fb7984298740153b05
Mauro Báez <mabaez@itba.edu.ar>
2022-10-19 16:44:46 -0300

    Semaphores

---
+++ Kernel/include/semaphore.h
@@ -0,0 +1,29 @@
+
+
+#include <ddlADT.h>
+#include <memoryManager.h>
+
+typedef struct blockHub
+{
+    int globalid; // se inicializa en 0 al principio de todo, también podría ser un static
+    ddlADT semBlockList;
+} blockHub;
+
+typedef struct semBlock
+{
+    char name[32];
+    int id;
+    int value;
+    ddlADT blockedProcessList;
+    semPointer next;
+} semBlock;
+
+typedef semBlock * semPointer;
+
+void initSemaphoreHub();
+
+int initializeSemaphore(char * name, int initialValue);
+
+int wait(int id);
+
+void post(int id);
---
+++ Kernel/semaphore.c
@@ -0,0 +1,99 @@
+#define NULL 0
+#include <semaphore.h>
+
+typedef struct blockHub
+{
+    int globalid; // se inicializa en 0 al principio de todo, también podría ser un static
+    ddlADT semBlockList;
+} blockHub;
+
+typedef struct semBlock
+{
+    char name[32];
+    int id;
+    int value;
+    ddlADT blockedProcessList;
+    semPointer next;
+} semBlock;
+
+typedef semBlock * semPointer;
+
+static char initialized;
+static blockHub hub; 
+
+void initSemaphoreHub()
+{
+    if(initialized)
+        return;
+    hub.globalid = 0;
+    initialized = 1;
+    hub.semBlockList = newList();
+}
+
+// si dos hacen el initialize al mismo tiempo se rompe por el toBegin
+// habría que hacer un find, o directamente no usar adt
+int initializeSemaphore(char * name, int initialValue) 
+{
+    toBegin(hub.semBlockList);
+    semPointer curr;
+    while((curr = (semPointer) next(hub.semBlockList))!=NULL)
+    {
+        if(!strCmp(curr->name, name))
+        {
+            return curr->id;
+        }
+    }
+    curr = memalloc(sizeof(struct semBlock));
+    strncpy(curr->name, name, 32);
+    curr->next = NULL;
+    curr->id = hub.globalid++;
+    curr->value = initialValue;
+    curr->blockedProcessList = newList();
+    add(hub.semBlockList, curr);
+    return curr->id;
+}
+
+int wait(int id)
+{
+    toBegin(hub.semBlockList);
+    semPointer curr;
+    while((curr = (semPointer) next(hub.semBlockList))!=NULL)
+    {
+        if(curr->id == id)
+        {
+            // hay que hacer lo de xchg
+            if(curr->value) 
+                curr->value--;
+            else{
+                int pid = getPid();
+                add(curr->blockedProcessList, pid);
+                blockProcess(pid);
+            }
+            return curr->value;
+        }
+    }
+    return -1;
+}
+
+void post(int id)
+{
+    toBegin(hub.semBlockList);
+    semPointer curr;
+    while((curr = (semPointer) next(hub.semBlockList))!=NULL)
+    {
+        if(curr->id == id)
+        {
+            // hay que hacer lo de xchg
+            if(!curr->value)
+            {
+                toBegin(curr->blockedProcessList);
+                int * pid = (int *) next(curr->blockedProcessList);
+                if(pid!=NULL){
+                    remove(curr->blockedProcessList);
+                    unblockProcess(*pid);
+                }
+            }
+            curr->value++;
+        }
+    }
+}
